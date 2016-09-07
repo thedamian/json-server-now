61c34ccffaac99b31bcdcad91ec9aea65e182062
Damian Montero <photoca@hotmail.com>
2016-09-07 08:27:38 -0400

    Adding Faker to the project

--- faker.js
+++ faker.js
@@ -2,11 +2,12 @@ module.exports = function() {
   var faker = require("faker");
   var _ = require("lodash");
   return {
-	people: _.times(100, function (n) {
-		id: n,
-		name: faker.name.findName(),
-		avatar: faker.internet.avater()
+			people: _.times(100, function (n) {
+				return {
+				id: n,
+				name: faker.name.findName(),
+				avatar: faker.internet.avatar()
+				}
+			})
 	}
-	})
-   }
 }
